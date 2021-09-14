890d70ca687195a4b80c35df9b4d733fe8b4252e
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-09-14 16:28:52 +0700

    В App List заменен на ListContainer

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import List from '../list/list';
 import store from '../../ts/store/store';
+import ListContainer from '../list/list-container';
 
 const root = document.querySelector('.app');
 
 if (root !== null) {
   ReactDOM.render(
     <Provider store={store}>
-      <List />
+      <ListContainer />
     </Provider>,
     root
   );
