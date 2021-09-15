0354a029dd06290d358ecd1da8d3d93cafd071c9
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-09-15 14:22:31 +0700

    Добавлены события для перемещения элемента списка

--- src/components/item/item.tsx
+++ src/components/item/item.tsx
@@ -1,8 +1,42 @@
 import React from 'react';
 import { IItem } from '../../ts/interfaces';
 
+// function handleItemDrag(event: React.DragEvent) {
+//   const target = event.target;
+
+//   if (target instanceof HTMLDivElement) {
+//     target.classList.add('item_transparent');
+//   }
+// }
+
+function handleItemDragStart(event: React.DragEvent) {
+  // const target = event.target;
+
+  // if (target instanceof HTMLDivElement) {
+  //   target.classList.add('item_transparent')
+  // }
+}
+
+function handleItemDragEnd(event: React.DragEvent) {
+  const target = event.target;
+
+  if (target instanceof HTMLDivElement) {
+    target.classList.remove('item_transparent');
+  }
+}
+
 const Item = (item: IItem): JSX.Element => {
-  return <div className="item">{item.text}</div>;
+  return (
+    <div
+      className="item"
+      draggable="true"
+      // onDrag={handleItemDrag}
+      onDragStart={handleItemDragStart}
+      onDragEnd={handleItemDragEnd}
+    >
+      {item.text}
+    </div>
+  );
 };
 
 export default Item;
