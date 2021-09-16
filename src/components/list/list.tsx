865ba60edc737030b4e0d3b5a385e31abd64152c
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-09-16 09:59:24 +0700

    Удален лишний код из List

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -3,7 +3,6 @@ import { IList } from '../../ts/interfaces';
 import Item from '../item/item';
 
 const List = (list: IList): JSX.Element => {
-  console.dir(list.items);
   const items = list.items.map((item) => {
     return (
       <Item
