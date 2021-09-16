9cb3d14753cfeee0cd496bac164d5fe46545892f
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-09-16 08:52:58 +0700

    Добавлен mapDispatchToProps

--- src/components/list/list-container.tsx
+++ src/components/list/list-container.tsx
@@ -1,11 +1,20 @@
 import { connect } from 'react-redux';
-import { IState } from '../../ts/interfaces';
+import { IDispatch, IState } from '../../ts/interfaces';
 import List from './list';
+import { itemIsDragCreator } from '../../ts/store/reducers/list-reducer';
 
 const mapStateToProps = (state: IState) => {
   return { items: state.list.items };
 };
 
-const ListContainer = connect(mapStateToProps)(List);
+const mapDispatchToProps = (dispatch: IDispatch) => {
+  return {
+    dragItem: (id: number) => {
+      dispatch(itemIsDragCreator(id));
+    },
+  };
+};
+
+const ListContainer = connect(mapStateToProps, mapDispatchToProps)(List);
 
 export default ListContainer;
