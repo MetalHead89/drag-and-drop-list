985ae3f5b66f28f572bb8de77049bebe741d4092
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-09-22 10:13:49 +0700

    Из компонента Item удален старый код, и закомментирован пока ненужный код

--- src/components/item/item.tsx
+++ src/components/item/item.tsx
@@ -1,91 +1,73 @@
 import React from 'react';
 import { IItem } from '../../ts/interfaces';
 
-// function handleItemDrag(event: React.DragEvent) {
+// function handleItemDragEnter(event: React.DragEvent) {
 //   const target = event.target;
 
 //   if (target instanceof HTMLDivElement) {
-//     target.classList.add('item_transparent');
+//     target.style.border = 'black 1px solid';
 //   }
 // }
 
-// function handleItemDragStart(event: React.DragEvent) {
-//   // const target = event.target;
-//   // if (target instanceof HTMLDivElement) {
-//   //   target.classList.add('item_transparent')
-//   // }
-// }
-
-function handleItemDragEnter(event: React.DragEvent) {
-  const target = event.target;
-
-  if (target instanceof HTMLDivElement) {
-    target.style.border = 'black 1px solid';
-  }
-}
-
-function handleItemDragLeave(event: React.DragEvent) {
-  const target = event.target;
-
-  if (target instanceof HTMLDivElement) {
-    target.style.border = 'none';
-  }
-}
-
-// function handleItemDragEnd(event: React.DragEvent) {
+// function handleItemDragLeave(event: React.DragEvent) {
 //   const target = event.target;
 
 //   if (target instanceof HTMLDivElement) {
-//     target.classList.remove('item_transparent');
+//     target.style.border = 'none';
 //   }
 // }
 
 const Item = (item: IItem): JSX.Element => {
   const handleItemDragStart = (event: React.DragEvent) => {
-    const target = event.target;
+    // const target = event.target;
 
-    if (target instanceof HTMLDivElement) {
-      target.classList.remove('item_transparent');
-      event.dataTransfer.effectAllowed = 'move';
-      event.dataTransfer.setData('text/html', target.innerHTML);
+    // if (target instanceof HTMLDivElement) {
+    //   target.classList.remove('item_transparent');
+    //   event.dataTransfer.effectAllowed = 'move';
+    //   event.dataTransfer.setData('text/html', target.innerHTML);
 
-      item.dragItem(item.id);
-    }
+    //   item.dragItem(item.id);
+    // }
   };
 
   const handleItemDragOver = (event: React.DragEvent) => {
     event.preventDefault();
-  };
-
-  const handleItemDrop = (event: React.DragEvent) => {
     event.stopPropagation();
-
-    const target = event.target;
-
-    if (target instanceof HTMLDivElement) {
-      target.innerHTML = event.dataTransfer.getData('text/html');
-    }
+    // event.dataTransfer.dropEffect = 'move';
   };
 
   function handleItemDragEnd(event: React.DragEvent) {
-    const target = event.target;
+    event.preventDefault();
+    event.stopPropagation();
+    // const target = event.target;
 
-    if (target instanceof HTMLDivElement) {
-      target.classList.remove('item_transparent');
-    }
+    // if (target instanceof HTMLDivElement) {
+    //   target.classList.remove('item_transparent');
+    // }
 
-    item.itemReleased(item.id);
+    // item.itemReleased(item.id);
   }
+
+  const handleItemDrop = (event: React.DragEvent) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    // const target = event.target;
+
+    // if (target instanceof HTMLDivElement) {
+    //   target.innerHTML = event.dataTransfer.getData('text/html');
+    // }
+  };
 
   return (
     <div
       className="item"
       draggable="true"
       // onDrag={handleItemDrag}
-      onDragEnter={handleItemDragEnter}
+      // onDragEnter={handleItemDragEnter}
       onDragOver={handleItemDragOver}
       onDrop={handleItemDrop}
-      onDragLeave={handleItemDragLeave}
+      // onDragLeave={handleItemDragLeave}
       onDragStart={handleItemDragStart}
       onDragEnd={handleItemDragEnd}
     >
